ba8525ad8dcd17528edae6d2157e0460d8d21e1f
Toke Friis <tokefriis@gmail.com>
2020-06-21 16:08:39 +0200

    man kan skrive text

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 import pygame
 
+pygame.init()
 width = 800
 height = 600
 # WINDOW
-win = pygame.display.set_mode((width, height))
+win = pygame.display.set_mode([width, height])
 # 'TITLE'
 pygame.display.set_caption('Shooting Game')
-
-
-def redraw_window(win):
-    # White
-    win.fill((255, 255, 255))
-    pygame.display.update()
+base_font = pygame.font.Font(None, 32)
 
 
 def main():
+    user_text = ''
+    clock = pygame.time.Clock()
     run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-        redraw_window(win)
+            if event.type == pygame.KEYDOWN:
+                user_text += event.unicode
+        win.fill((255, 255, 255))
+        text_surface = base_font.render(user_text, True, (0, 0, 0))
+        win.blit(text_surface, (0, 0))
+        pygame.display.flip()
+        clock.tick(60)
 
 
 main()
