00ad4531639be3ed53badd41f0026d40de76424e
Toke Friis <tokefriis@gmail.com>
2020-06-21 17:00:51 +0200

    har lavet en firkant

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ height = 600
 win = pygame.display.set_mode([width, height])
 # 'TITLE'
 pygame.display.set_caption('Shooting Game')
-base_font = pygame.font.Font(None, 32)
 
 
 def main():
+    color = pygame.Color('black')
+
+    username_rect = pygame.Rect(200, 200, 140, 32)
+    title_font = pygame.font.Font('freesansbold.ttf', 50)
+    base_font = pygame.font.Font(None, 32)
     user_text = ''
     clock = pygame.time.Clock()
     run = True
@@ -20,12 +24,22 @@ def main():
                 pygame.quit()
                 exit()
             if event.type == pygame.KEYDOWN:
-                user_text += event.unicode
+                if event.key == pygame.K_BACKSPACE:
+                    user_text = user_text[:-1]
+                else:
+                    user_text += event.unicode
         win.fill((255, 255, 255))
-        text_surface = base_font.render(user_text, True, (0, 0, 0))
-        win.blit(text_surface, (0, 0))
+
+        string = title_font.render('Login', True, (255, 132, 27))
+        username = base_font.render(user_text, True, (0, 0, 0))
+        win.blit(string, (width//2 - 70, 50))
+        win.blit(username, (width//4, height//4))
+
+        pygame.draw.rect(win, color, username_rect, 2)
         pygame.display.flip()
         clock.tick(60)
+    print(user_text)
+base_font = pygame.font.Font(None, 32)
 
 
 main()
